686c329cbe41c12495ea9117d3a632e49dd5ffa7
kkstas <stasiakkamilkrzysztof@gmail.com>
2023-12-24 12:40:13 +0100

    Add InsertAfter() test

--- linked_list_test.go
+++ linked_list_test.go
@@ -70,3 +70,61 @@ func TestFindByIndex(t *testing.T) {
 		}
 	}
 }
+
+
+
+func TestInsertAfter(t *testing.T) {
+    afterT1 := "after t1"
+    afterT2 := "after t2"
+    afterT3 := "after t3"
+
+	list := LinkedList{}
+	list.Add("t1")
+	list.Add("t2")
+	list.Add("t3")
+
+
+    list.InsertAfter("t1", afterT1)
+    wantT1 := afterT1
+    gotT1result, err := list.Find("t1")
+    if err != nil {
+			t.Errorf("error returned: %q", err)
+        return
+    }
+
+    gotT1 := gotT1result.next.value
+
+    if gotT1 != wantT1 {
+        t.Errorf("got %q, wanted %q", gotT1, wantT1)
+    }
+
+    list.InsertAfter("t2", afterT2)
+    wantT2 := afterT2
+    gotT2result, err := list.Find("t2")
+    if err != nil {
+			t.Errorf("error returned: %q", err)
+        return
+    }
+
+    gotT2 := gotT2result.next.value
+
+    if gotT2 != wantT2 {
+        t.Errorf("got %q, wanted %q", gotT2, wantT2)
+    }
+
+
+    list.InsertAfter("t3", afterT3)
+    wantT3 := afterT3
+    gotT3result, err := list.Find("t3")
+    if err != nil {
+			t.Errorf("error returned: %q", err)
+        return
+    }
+
+    gotT3 := gotT3result.next.value
+
+    if gotT3 != wantT3 {
+        t.Errorf("got %q, wanted %q", gotT3, wantT3)
+    }
+
+}
